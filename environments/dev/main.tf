548df9276032b9b3dbbe75838ebe2bcd2e53f6b0
stefangsalandy <64261155+stefangsalandy@users.noreply.github.com>
2020-11-18 08:26:43 -0500

    Update main.tf
    Data Lake CI/CD Testing 3 by SGS

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# CI/CD Testing - 2
+# CI/CD Testing - 3 by SGS
 
 terraform {
   required_version = ">= 0.12.0"
